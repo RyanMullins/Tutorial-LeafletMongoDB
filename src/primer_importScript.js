59ea752d35de729c24b60c312ecbfe6bf55cdcf5
Ryan Mullins <rsm5068@gmail.com>
2013-09-27 09:07:59 -0400

    Added primer tweets and import script for those starting without an existing collection of tweets.

---
+++ src/primer_importScript.js
@@ -0,0 +1,23 @@
+var mongo = require('mongodb');
+var tweets = require('./primer_tweets.json');
+
+
+var mdbServer = mongo.Server('localhost', 27017, {'auto_reconnect' : true});
+var mdb = mongo.Db('streaming_db', mdbServer);
+
+mdb.open(function (err, db) {
+    db.createCollection('tweets', function (err, collection) {
+        for (var i = 0; i < tweets.length; i++) {
+            var tweet = tweets[i];
+            tweet._id = new mongo.Long.fromString(tweet.id_str, 10);
+            collection.insert(tweet, function (err, doc) {
+                if (err) { 
+                    console.log("Error writing document to database. Most likely a duplicate.");
+                }
+            });
+        }
+
+        db.close();
+        process.exit(0);
+    });
+});
